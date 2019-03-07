cb89e9532761fe7906203750c454743feb532e6c
Eddy Montus <eddy@helloasso.com>
2019-03-07 17:23:47 +0100

    update wording

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ var welcomeList = ["Hello", "Bonjour", "Ca fart ?", "How are u doing ?"];
 var firstName = "";
 var lastName = "";
 var sex = "";
+var email = "";
 var price = "";
 
-var sexValues = ["F", "M", "O"];
+var sexValues = [
+    "Femme",
+    "Homme",
+    "Autre"
+];
 
 var eventValues = [
   "Caritatif",
@@ -23,7 +28,7 @@ var eventValues = [
 console.log(Utils.randomize(welcomeList));
 
 // Ask name
-var answer = readlineSync.question("May I have your name? ").toUpperCase();
+var answer = readlineSync.question("Merci d'entrer votre nom et prénom séparé par une virgule(,) : ").toUpperCase();
 var nameParts = answer.split(",");
 firstName = nameParts[0];
 lastName = nameParts[1];
@@ -31,12 +36,12 @@ lastName = nameParts[1];
 // Check genre
 var sexAnswer = -1;
 while (sexAnswer < 0) {
-  sexAnswer = readlineSync.keyInSelect(sexValues, "May I have your sex ? ");
+  sexAnswer = readlineSync.keyInSelect(sexValues, "De quel sexe êtes-vous ?");
 }
 sex = sexValues[sexAnswer];
 
 // Ask for email
-var email = readlineSync.questionEMail("Votre courriel please :");
+email = readlineSync.questionEMail("Votre adresse email : ");
 
 // Ask for evenType until a good answer is done
 var eventAnswer = -1;
@@ -49,11 +54,10 @@ while (eventAnswer < 0) {
 eventType = eventValues[eventAnswer];
 
 // Ask for $$$
-var priceAnswer = readlineSync.questionFloat("Combien tu payes ?");
-price = priceAnswer;
+price = readlineSync.questionFloat("Tarif : ");
 
 //Recap
 console.log(
-  `${firstName} ${lastName} va payer ${price} € pour un évènement ${eventType}`
+  `${firstName} ${lastName} de sexe ${sex} va payer ${price} € pour un évènement ${eventType}`
 );
 console.log(`Un Email sera envoyé à : ${email}`);
